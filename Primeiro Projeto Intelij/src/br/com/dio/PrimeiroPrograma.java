2bc01de0644aadf0efc9ecaca6fb9430f139b354
Alexandre Leque <89885076+AlexandreLeque@users.noreply.github.com>
2021-09-06 22:53:28 -0400

    Criando class livro

--- Primeiro Projeto Intelij/src/br/com/dio/PrimeiroPrograma.java
+++ Primeiro Projeto Intelij/src/br/com/dio/PrimeiroPrograma.java
@@ -2,11 +2,17 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
+      //  private static Object Livro;
+        //private static Object Livro;
+
         public static void main(String[] args) {
 
                 Gato gato = new Gato();
                 System.out.println(gato);
 
-        }
+
+       }
 }
